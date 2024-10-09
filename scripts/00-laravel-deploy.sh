8a334ba76e52dd31c1b237e8d826cc41ead6d3e6
LuisEduardoDev1 <luisedunole@gmail.com>
2024-10-09 10:55:35 -0300

    Mais testes

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer install --no-dev --working-dir=/var/www/html
+composer global require hirak/prestissimo
+composer install
 
 echo "Caching config..."
 php artisan config:cache
@@ -9,7 +10,5 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force 
 
-echo "Publishing cloudinary provider..."
-php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
+php artisan migrate --force
